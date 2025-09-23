849c6cbf8642ad08243c6427bcd4262d960b031a
Roman Czerwinski <romanczerwinski@r0m4n.com>
2025-09-23 09:00:37 -0500

    made the button return to normal after 5s

--- app/(auth)/passwordLogin.tsx
+++ app/(auth)/passwordLogin.tsx
@@ -1,13 +1,25 @@
-import { JSX, useState } from 'react';
+import { JSX, useState, useEffect } from 'react';
 import { Br, Button, ThemedInput, ThemedText, ThemedView } from '@/components';
 import { viewStyles } from '@/styles/view';
 import { useUserContext } from '@/contexts';
 
 export default function PasswordLogin(): JSX.Element {
   const [disabled, setDisabled] = useState(false);
+  const [loadingDots, setLoadingDots] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const { logIn } = useUserContext();
+
+  useEffect(() => {
+    if (!disabled) return;
+    setTimeout(() => {
+      if (loadingDots.length >= 3) {
+        setLoadingDots('');
+        return;
+      }
+      setLoadingDots(loadingDots + '.');
+    }, 500);
+  }, [disabled, loadingDots]);
 
   return (
     <ThemedView style={viewStyles.view}>
@@ -23,9 +35,10 @@ export default function PasswordLogin(): JSX.Element {
         onPress={async () => {
           setDisabled(true);
           await logIn({ email, password });
+          setTimeout(() => setDisabled(false), 5_000);
         }}
         disabled={disabled}>
-        {disabled ? 'Loading...' : 'Sign In'}
+        {disabled ? `Loading${loadingDots}` : 'Sign In'}
       </Button>
     </ThemedView>
   );
